8bcf8233961fc4ca4023f7f049491e40c525f5f7
+ <+>
2023-03-28 11:47:02 -0400

    need to study more

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
 // write your code here
+fetch("http://localhost:3000/ramens")
+  .then((response) => response.json())
+  .then((ramens) => {
+    // data === ramens
+    ramens.forEach((ramen) => {
+      rendertheRamens(ramen); // iterate through the data of ramen one by one
+    });
+  });
+
+// global scope for 'menu'
+let menu = document.getElementById("main-menu");
+
+// I don't know if its normal function or arrow function
+// const rendertheRamens = (ramen) => {
+function rendertheRamens(ramen) {
+  // iterating through the ramen data to do things with it
+  // iterate to get the image and paste it on the menu bar
+  let img = document.createElement("img"); // creating image tag
+  img.src = ramen.image;
+  // appending the images to the menu bar
+  menu.append(img);
+
+  // add an event listener when an image is clicked
+  img.addEventListener("click", () => {
+    let ramenimage = document.getElementsByClassName("detail-image");
+    ramenimage.src = ramen.image;
+
+    let name = document.getElementsByClassName("name");
+    name.innerHTML = ramen.name;
+
+    let restaurant = document.getElementsByClassName("restaurant");
+    restaurant.innerHTML = ramen.restaurant;
+
+    let rating = document.getElementById("rating-display");
+    rating.innerHTML = ramen.rating;
+
+    let comment = document.getElementById("comment-display");
+    comment.textContent = ramen.comment;
+  });
+}
+
+/* // POST request 
+    fetch('http://localhost:3000/ramens', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+
+         })
+    })
+
+*/
